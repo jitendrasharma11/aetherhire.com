9c3c13b2e0f8e131f63b7346d18fde536b900470
Jitendra Sharma <jitendrasharma11>
2025-03-17 18:21:29 +0530

    aetherhire

---
+++ js/app.js
@@ -0,0 +1,39 @@
+$('#industries-slider').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:1000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
+
+$('#testimonial-slider').owlCarousel({
+    loop:true,
+    margin:30,
+    nav:false,
+    dots:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:2
+        }
+    }
+})
